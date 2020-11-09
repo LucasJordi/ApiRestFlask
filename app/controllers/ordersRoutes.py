eb73cc2e36aaadf88710e1923036d8167a7decf5
LucasJordi <lucasjordi24@gmail.com>
2020-11-09 10:14:01 -0300

    Update ordersRoutes.py

--- app/controllers/ordersRoutes.py
+++ app/controllers/ordersRoutes.py
@@ -6,8 +6,7 @@ from .tables.products import *
 
 
 
-#Rotas para o cadastro de ordens de compra
-#A primeira etapa é o de adicionar as ordens ao carrinho
+#Add produtc in a order list
 
 @app.route('/orders/createorder/<int:product_id>')
 def CreateOrder(product_id):
@@ -18,31 +17,40 @@ def CreateOrder(product_id):
     Price=float(productSearch[0]['price'])
     allPrices.append(Price)
     totalPrice=sum(allPrices)
-    orderNumber=len(orders)+1
+    orderId=len(orders)+1
+
+    
+
     
     
     
     
 
-    return jsonify({'Order number':orderNumber,'Products':orderSelect,"Total price":totalPrice})
+    return jsonify({'Order number':orderId,'Products':orderSelect,"Total price":totalPrice})
 
 
-
+#Submit order 
 
 @app.route('/orders/createorder/submit')
 def SubmitOrder():
     
-    orders.append(copy.copy(orderSelect))
+    orders.append(orderSelect[:])
+    
+
+    
     orderSelect.clear()   
 
     return 'Submit!'
 
+#View a especific order
 
 @app.route('/orders/<int:item_id>')
 def ChangeItem(item_id):
     item_search=[item for item in orderSelect['id']==item_id]
     return jsonify(item_search)
 
+
+#View all orders
 
 @app.route('/orders/all')
 def FindOrder():
